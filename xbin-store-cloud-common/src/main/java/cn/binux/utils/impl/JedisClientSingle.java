d5976f275191b290bbd05d68b51ddec9f2f2de49
wangketao2010 <wangketao2010@qq.com>
2019-01-13 16:36:17 +0800

    这个代码没有完全实现功能只能访问个首页，页面还是直接弄下来某东的页面，没意思了不玩了

--- xbin-store-cloud-common/src/main/java/cn/binux/utils/impl/JedisClientSingle.java
+++ xbin-store-cloud-common/src/main/java/cn/binux/utils/impl/JedisClientSingle.java
@@ -14,16 +14,16 @@ import redis.clients.jedis.JedisPool;
  */
 public class JedisClientSingle implements JedisClient {
 
-    @Autowired
-    JedisPoolWrapper jedisPoolWrapper;
 //    @Autowired
-//    private JedisPool jedisPool;
+//    JedisPoolWrapper jedisPoolWrapper;
+    @Autowired
+    private JedisPool jedisPool;
 
     @Value("${redis.password}")
     private String password;
 
     private Jedis getResource() {
-        JedisPool jedisPool = jedisPoolWrapper.getJedisPool();
+//        JedisPool jedisPool = jedisPoolWrapper.getJedisPool();
         Jedis resource = jedisPool.getResource();
         if (StringUtils.isBlank(password)) {
             return resource;
--- xbin-store-cloud-service-cart/src/test/java/cn/binux/XbinStoreServiceCartApplicationTests.java
+++ xbin-store-cloud-service-cart/src/test/java/cn/binux/XbinStoreServiceCartApplicationTests.java
@@ -1,16 +1,34 @@
 package cn.binux;
 
+import cn.binux.cart.service.CartService;
+import cn.binux.cart.service.impl.CartServiceImpl;
+import cn.binux.utils.JedisClient;
+import cn.binux.utils.impl.JedisClientSingle;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Configuration
+@MapperScan(basePackages = "cn.binux.mapper")
 public class XbinStoreServiceCartApplicationTests {
+    @Autowired
+    private CartServiceImpl cartService;
 
     @Test
     public void contextLoads() {
+        cartService.getCartInfoListByCookiesId("abcd");
     }
-
+    public JedisClient jedisClient() {
+        return new JedisClientSingle();
+    }
 }
